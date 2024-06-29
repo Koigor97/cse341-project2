449181bc82d1bfef7c2f33e8f84f861775127b99
Koigor97 <turaysamuel101@yahoo.com>
2024-06-29 22:33:17 +0200

    refactor tvShowController

--- controllers/tvShowsController.js
+++ controllers/tvShowsController.js
@@ -1,7 +1,7 @@
 //* Defining business logic for tv-shows collections
 
 const TvShow = require('../models/tvShowModel');
-const { convertQueryParams } = require('../utils/helpers');
+const QueryAPIFeatures = require('../utils/queryFeatures');
 //********************* CONTROLLERS ****************** */
 
 /**
@@ -13,66 +13,13 @@ const { convertQueryParams } = require('../utils/helpers');
  */
 exports.getTvShows = async (req, res) => {
   try {
-    // TODO: BUILD THE QUERY
-    // 1.a) FILTERING
-    /**
-     * * in order to separate the queries from filters,
-     * * I would create a shallow copy of the query object
-     * * and create an array of fields to exclude from the query
-     */
-
-    const queryObj = { ...req.query };
-
-    //* fields to exclude from the query
-    const excludeFields = ['page', 'sort', 'limit', 'fields'];
-    excludeFields.forEach((el) => delete queryObj[el]);
-
-    console.log(`--------------OBJECT TO QUERY--------------------`);
-    console.log(`object to query:`, queryObj);
-
-    // 1.b) ADVANCED FILTERING
-    /**
-     * * the code below returns a query promise. The query promise
-     * * allows us to chain queries using mongoose Model methods
-     * * like find, findOne, etc. e.g: TvShow.find({}).limit(10).sort({date: -1})
-     * * that is why I omit the `await` keyword, because it would
-     * * resolve the promise immediately and returns the data.
-     */
-
-    //* convert query parameters to MongoDB-compatible query objects
-    const processedQuery = convertQueryParams(queryObj);
-    let query = TvShow.find(processedQuery);
-
-    // 2) SORTING
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      console.log(`sort by:`, sortBy);
-      query = query.sort(sortBy);
-    } else {
-      query = query.sort('-updated');
-    }
-
-    // 3) FIELD LIMITING
-    if (req.query.fields) {
-      console.log(`fields:`, req.query.fields);
-      const fields = req.query.fields.split(',').join(' ');
-      console.log(`fields:`, fields);
-      query = query.select(fields);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 4) PAGINATION
-    const page = req.query.page * 1 || 1;
-    console.log(`page:`, page);
-    const limit = req.query.limit * 1 || 10;
-    console.log(`limit:`, limit);
-    const skip = (page - 1) * limit;
-    console.log(`skip:`, skip);
-    query = query.skip(skip).limit(limit);
-
     // TODO: EXECUTE THE QUERY
-    const tvShows = await query;
+    const features = new QueryAPIFeatures(TvShow.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tvShows = await features.query;
 
     // TODO: SEND RESPONSE
     res
@@ -144,6 +91,39 @@ exports.deleteTvShow = async (req, res) => {
   try {
     const tvShow = await TvShow.findByIdAndDelete(req.params.id);
     res.status(204).json({ message: 'Deleted successfully' });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+//* --------------AGGREGATION PIPELINE--------------------- //
+
+/**
+ *
+ * * Get stats for TV shows with rating greater than or equal to 6.5
+ *
+ * * @param {*} req
+ * * @param {*} res
+ */
+
+exports.getTvShowStats = async (req, res) => {
+  try {
+    const stats = await TvShow.aggregate([
+      {
+        $match: { 'rating.average': { $gte: 6.5 } }
+      },
+      {
+        $group: {
+          _id: '$network.country.name',
+          averageRating: { $avg: '$rating.average' },
+          totalRating: { $sum: '$rating.count' },
+          maxRating: { $max: '$rating.average' },
+          minRating: { $min: '$rating.average' }
+        }
+      }
+    ]);
+
+    res.status(200).json({ status: 'success', data: { stats } });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
