b6f486b9ae730ed1464ab13461ad265618346737
Koigor97 <turaysamuel101@yahoo.com>
2024-06-30 19:39:25 +0200

    add swagger tags to userController function

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -11,7 +11,10 @@ const User = require('../models/userModel');
  ** @param {Object} res - The response object.
  ** @return {Promise<void>} A promise that resolves when the response is sent.
  */
+
 exports.getUsers = async (req, res) => {
+  // #swagger.tags = ['Users']
+
   try {
     const users = await User.find(req.query);
     res
@@ -28,7 +31,10 @@ exports.getUsers = async (req, res) => {
  ** @param {Object} res - The response object.
  ** @return {Promise<void>} A promise that resolves when the response is sent.
  */
+
 exports.getUser = async (req, res) => {
+  // #swagger.tags = ['Users']
+
   try {
     const user = await User.findById(req.params.id);
     res.status(200).json({ status: 'success', data: { user } });
@@ -44,7 +50,10 @@ exports.getUser = async (req, res) => {
  ** @param {Object} res - The response object.
  ** @return {Promise<void>} A promise that resolves when the response is sent.
  */
+
 exports.createUser = async (req, res) => {
+  // #swagger.tags = ['Users']
+
   try {
     const user = await User.create(req.body);
     res.status(201).json({ status: 'success', data: { user } });
@@ -60,7 +69,10 @@ exports.createUser = async (req, res) => {
  ** @param {Object} res - The response object.
  ** @return {Promise<void>} A promise that resolves when the response is sent.
  */
+
 exports.updateUser = async (req, res) => {
+  // #swagger.tags = ['Users']
+
   try {
     const user = await User.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
@@ -79,7 +91,10 @@ exports.updateUser = async (req, res) => {
  ** @param {Object} res - The response object.
  ** @return {Promise<void>} A promise that resolves when the response is sent.
  */
+
 exports.deleteUser = async (req, res) => {
+  // #swagger.tags = ['Users']
+
   try {
     const user = await User.findByIdAndDelete(req.params.id);
     res.status(200).json({ status: 'success', data: { user } });
