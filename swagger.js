55229cd0b80262b3a80eeb501988e9e11aaca35b
Koigor97 <turaysamuel101@yahoo.com>
2024-06-30 19:37:15 +0200

    create swagger.js file

---
+++ swagger.js
@@ -0,0 +1,24 @@
+//* using swagger autogen to generate swagger docs
+
+const swaggerAutogen = require('swagger-autogen')();
+
+//* specify the output folder
+const outputFile = './models/swagger_output.json';
+
+//* specify the endpoints
+const endpointsFiles = ['./routes/tvShowsRoutes'];
+
+//* defining a starter doc object: optional
+const doc = {
+  info: {
+    version: '1.0.0',
+    title: 'TV Shows API',
+    description: 'API for TV Shows'
+  },
+  host: 'localhost:8000',
+  schemes: ['http'],
+  swagger: '2.0'
+};
+
+//* generate swagger docs
+swaggerAutogen(outputFile, endpointsFiles, doc);
