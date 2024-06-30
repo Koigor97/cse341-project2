beb8ce0b1dd82920888cda705a3fa8d049babe90
Koigor97 <turaysamuel101@yahoo.com>
2024-06-30 19:41:31 +0200

    create appErrorClass for catching errors globally

---
+++ utils/appErrorClass.js
@@ -0,0 +1,15 @@
+//* Handling global application errors using
+//* built in Error class from Node.js
+
+class AppErrorClass extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = AppErrorClass;
