4ed90006aee86f6b11db6129ce5999d2e7b9df79
Koigor97 <turaysamuel101@yahoo.com>
2024-07-02 16:07:14 +0200

    create a error handling middleware file

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,13 +1,60 @@
 //* Error handler middleware file
+const dotenv = require('dotenv');
+dotenv.config();
+const AppErrorClass = require('../utils/appErrorClass');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppErrorClass(message, 400);
+};
+
+const errorHandlerDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const errorHandlerProd = (err, res) => {
+  /***
+   * * Checks if the error is an Operational Error
+   * * which is defined in the AppErrorClass. If it is
+   * * then it sends a JSON response with the status
+   * * code, status, and message from the error. And if
+   * * it is not then it sends a generic response with a
+   * * status code of 500 and an error message
+   */
+
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    console.log('Error 😵‍💫', err);
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!'
+    });
+  }
+};
 
 exports.errorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    errorHandlerDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+
+    errorHandlerProd(error, res);
+  }
 };
 
 exports.catchAsync = (fn) => {
