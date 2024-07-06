6979234ed0bab9305e00a81223943bf39d9fed61
Koigor97 <turaysamuel101@yahoo.com>
2024-07-06 18:44:57 +0200

    implement security middlewares

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const cors = require('cors');
 const morgan = require('morgan');
 const swaggerUI = require('swagger-ui-express');
 const swaggerDocument = require('./models/swagger_output.json');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppErrorClass = require('./utils/appErrorClass');
 const appErrorHandler = require('./middleware/errorHandler');
@@ -19,6 +24,18 @@ const usersRoutes = require('./routes/usersRoutes');
 //* instantiate express app
 const app = express();
 
+//* security middleware for HTTP headers
+app.use(helmet());
+
+const limiter = rateLimit({
+  max: 500,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+
+//* apply rate limiter for all routes.
+app.use('/api', limiter);
+
 //*************************** MIDDLEWARE ****************** */
 
 /**
@@ -28,9 +45,30 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(cors()); //? allows cross-origin resource sharing
-app.use(express.json()); //? parses incoming requests with JSON payloads
-app.use(express.urlencoded({ extended: false })); //? parses incoming requests with urlencoded payloads
+//? allows cross-origin resource sharing
+app.use(cors());
+
+//? parses incoming requests with JSON payloads
+app.use(express.json({ limit: '10kb' }));
+
+//? data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//? data sanitization against XSS
+app.use(xss());
+
+//? prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: ['genres', 'runtime', 'rating', 'externals']
+  })
+);
+
+//? serves static files
+app.use(express.static(`${__dirname}/public`));
+
+//? parses incoming requests with urlencoded payloads
+app.use(express.urlencoded({ extended: false }));
 
 //*************************** ROUTES ****************** */
 
