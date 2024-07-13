3a1c02e1f92e02d0f64494399caf6b7586e55331
Koigor97 <turaysamuel101@yahoo.com>
2024-07-13 11:51:32 +0200

    implement GoogleStrategy middleware object

--- middleware/passportConfig.js
+++ middleware/passportConfig.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const GitHubStrategy = require('passport-github2').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/userModel'); // Adjust the path if necessary
 
 passport.use(
@@ -33,6 +34,32 @@ passport.use(
         return done(null, user);
       } catch (err) {
         return done(err, false);
+      }
+    }
+  )
+);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (!user) {
+          user = await User.create({
+            googleId: profile.id,
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            image: profile.photos[0].value
+          });
+        }
+        return done(null, user);
+      } catch (error) {
+        return done(error, false);
       }
     }
   )
